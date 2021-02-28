319cd9099da38f3e0973b22daadbbcc0d8512454
cristopher <cristophermoreno92@gmail.com>
2021-02-28 18:23:43 -0500

    p.66 - Node + Express + Mongoose

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ const config = {
     env: process.env.NODE_ENV || 'development',
     port: process.env.PORT || 3000,
     jwtSecret: process.env.JWT_SECRET || 'YOUR_secret_key',
-    mongoUri: process.env.MONGODB_URI || 'mongodb://root:example@mongo_server:27017/menrproject',
+    mongoUri: process.env.MONGODB_URI || 'mongodb://mongo_server/menrproject',
 }
 
 export default config
