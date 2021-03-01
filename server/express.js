d4e06d0d748d730e04280e1596a3c4daaf780ff9
cristopher <cristophermoreno92@gmail.com>
2021-02-28 21:33:16 -0500

    p.70 - Password as a Virtual Field

--- server/express.js
+++ server/express.js
@@ -4,6 +4,7 @@ import cookieParser from 'cookie-parser'
 import compress from 'compression'
 import cors from 'cors'
 import helmet from 'helmet'
+import Template from './../template'
 
 const app = express()
 
@@ -13,5 +14,12 @@ app.use(cookieParser())
 app.use(compress())
 app.use(helmet())
 app.use(cors())
+
+//Nodemon is trackin: /server (folder)
+app.get('/', (req, res) => {
+    res.status(200).send(Template())
+})
+
+
 
 export default app
