0e4feb082e2fa258fc3049a767977bdc0c6f0080
cristopher <cristophermoreno92@gmail.com>
2021-03-06 21:59:52 -0500

    p.72 - Password Field Validation

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -36,14 +36,61 @@ const UserSchema = new mongoose.Schema({
 UserSchema.virtual('password').set(function (password) {
 
     this._password = password
-    
+
     this.salt = this.makeSalt()
 
     this.hashed_password = this.encryptPassword(password)
 
 }).get(function () {
+
     return (this._password)
+
 })
 
+UserSchema.methods = {
 
+    authenticate: function (plainText) {
+        return (this.encryptPassword(plainText) === this.hashed_password)
+    },
+
+    encryptPassword: function (password) {
+
+        if (!password) return ''
+
+        try {
+            return (
+                crypto.createHmac('sha1', this.salt).update(password).digest('hex')
+            )
+        } catch (error) {
+            return (error)
+        }
+
+    },
+
+    makeSalt: function () {
+
+        return Math.round((new Date().valueOf() * Math.random())) + ''
+
+    },
+}
+
+UserSchema.path('hashed_password').validate(function (v) {
+
+    if (this._password && this._password.length < 6) {
+
+        this.invalidate('password', 'Password must be at least 6 characters')
+
+    }
+
+    if (this.isNew && !this._password) {
+
+        this.invalidate('password', 'Password is required')
+
+    }
+
+}, null)
+
+
+
+//p.71 - PASSWORD FIELD VALIATION
 export default mongoose.model('User', UserSchema)
